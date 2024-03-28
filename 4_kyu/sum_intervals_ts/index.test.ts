c1da42c6db0825ead6eb74b3095a816fccdfe448
Mark Kovari <kovarimarkofficial@gmail.com>
2024-03-28 13:51:29 +0100

    chore: add typescript solution for 4 kyu sum intervals

---
+++ 4_kyu/sum_intervals_ts/index.test.ts
@@ -0,0 +1,16 @@
+import { assertEquals } from "https://deno.land/std@0.221.0/assert/mod.ts";
+import { sumOfIntervals } from './index.ts'
+
+const testCases: { input: [number, number][], expected: number }[] = [
+    { input: [[1, 5]], expected: 4 },
+    { input: [[1, 5], [6, 10]], expected: 8 },
+    { input: [[1, 5], [1, 5]], expected: 4 },
+    { input: [[1, 4], [7, 10], [3, 5]], expected: 7 },
+    { input: [[-1e9, 1e9]], expected: 2e9 },
+    { input: [[0, 20], [-1e8, 10], [30, 40]], expected: 1e8 + 30 },
+];
+for (const testCase of testCases) {
+    Deno.test(`Sum of intervals: ${testCase.input} => ${testCase.expected}`, () => {
+        assertEquals(sumOfIntervals(testCase.input), testCase.expected);
+    });
+}
---
+++ 4_kyu/sum_intervals_ts/index.ts
@@ -0,0 +1,19 @@
+export function sumOfIntervals(intervals: [number, number][]): number {
+    if (intervals.length === 0) return 0;
+    if (intervals.length === 1) return intervals[0][1] - intervals[0][0];
+    let result: number = 0;
+    intervals.sort(([a], [b]) => a - b);
+    const [first, ...rest] = intervals;
+    let [start, end] = first;
+    for (const [currentStart, currentEnd] of rest) {
+        if (currentStart > end) {
+            result += end - start;
+            start = currentStart;
+            end = currentEnd;
+        } else {
+            end = Math.max(end, currentEnd);
+        }
+    }
+    result += end - start;
+    return result;
+}
