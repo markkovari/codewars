a553511c107a322a7211c4f4bc1f7d517dea2c79
markkovari <kovarimarkofficial@gmail.com>
2024-08-21 14:49:39 +0200

    chore: refactor tests

--- 5_kyu/square_sums_simple/main.go
+++ 5_kyu/square_sums_simple/main.go
@@ -1,12 +1,20 @@
 package main
 
+import "math"
+
 func SquareSumsRow(n int) []int {
-	if n == 2 {
+	if n <= 2 {
 		return nil
 	}
-	result := make([]int, 1)
-	result[0] = 1
-	return result
+	return nil
+}
+
+func IsSqrt(aNumber int) bool {
+	if aNumber == 1 {
+		return false
+	}
+	theSquare := math.Floor(math.Sqrt(float64(aNumber)))
+	return math.Pow(theSquare, 2) == float64(aNumber)
 }
 
 func main() {
--- 5_kyu/square_sums_simple/main_test.go
+++ 5_kyu/square_sums_simple/main_test.go
@@ -3,18 +3,43 @@ package main
 import "testing"
 
 func TestSquareIncremental(t *testing.T) {
-	one := SquareSumsRow(1)
-	testOnceInArray(one, t)
-	if one[0] != 1 {
-		t.Fatalf("standalon element should be 1, got=%d", one[1])
+	for i := range []int{1, 2, 3} {
+		small := SquareSumsRow(i)
+		if small != nil {
+			t.Fatalf("Two elements cannot be solved with squares, instead got %+v", small)
+		}
 	}
-	two := SquareSumsRow(2)
-	if two != nil {
-		t.Fatalf("Two elements cannot be solved with squares, instead got %+v", two)
+
+}
+
+func TestIsSquare(t *testing.T) {
+
+	notSquares := []int{1, 2, 3, 5, 6, 11, 101}
+	squares := []int{4, 9, 16, 25, 36, 49, 81, 100}
+
+	for _, notSquare := range notSquares {
+		if IsSqrt(notSquare) {
+			t.Fatalf("%d should not be square", notSquare)
+		}
+	}
+
+	for _, square := range squares {
+		if !IsSqrt(square) {
+			t.Fatalf("%d should  be square", square)
+		}
 	}
 }
 
 func testOnceInArray(ar []int, t *testing.T) {
+	if ar == nil {
+		return
+	}
+	if len(ar) == 0 {
+		return
+	}
+	if len(ar) == 1 {
+		return
+	}
 	lookup := make(map[int]int)
 	for _, elem := range ar {
 		lookup[elem] += 1
